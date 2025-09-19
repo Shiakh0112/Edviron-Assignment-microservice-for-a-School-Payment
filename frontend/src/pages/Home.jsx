6b88f59c8f364bb145e5b5cd89586decf1458a63
ashfaq <khanashfaq9423@gmail.com>
2025-09-19 23:50:14 +0530

    Edviron  microservice for a School Payment and Dashboard Application Assignment complete with MERN STACK

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
             Login
           </Link>
           <Link
-            to="/signup"
+            to="/login"
             className="px-4 py-2 rounded-lg bg-green-500 text-white hover:bg-green-600"
           >
             Signup
